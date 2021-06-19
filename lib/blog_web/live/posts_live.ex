a2a8c0c89cec2f0229ca287bfde2baee9ef04302
Brooklin Myers <brooklinjazzmyers@gmail.com>
2021-06-19 13:03:46 -0400

    use client safe author get

--- lib/blog_web/live/posts_live.ex
+++ lib/blog_web/live/posts_live.ex
@@ -4,7 +4,7 @@ defmodule BlogWeb.PostsLive do
   alias Blog.Accounts
 
   def author_name(author_id) do
-    Accounts.get_author!(author_id).name
+    Accounts.get_client_safe_author!(author_id).name
   end
 
   def mount(_params, _session, socket) do
